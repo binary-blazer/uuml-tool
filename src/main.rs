d2a6f3a3d3477ca54b34c8cbb0c0ef0e291709aa
Jonas Franke <jonasfranke@sdevs.org>
2024-12-02 11:28:52 +0100

    Add initial implementation of uuml tool for replacing umlauts in HTML files

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use clap::{Arg, Command};
+use std::fs;
+
+fn main() {
+    let matches = Command::new("uuml")
+        .version("1.0")
+        .author("Jonas F. Franke <@jonasfranke@sdevs.org>")
+        .about("Replaces Umlauts with their HTML entities")
+        .arg(
+            Arg::new("file")
+                .short('f')
+                .long("file")
+                .value_name("FILE")
+                .help("Sets the HTML file to use")
+                .num_args(1),
+        )
+        .arg(
+            Arg::new("directory")
+                .short('d')
+                .long("directory")
+                .value_name("DIRECTORY")
+                .help("Sets the directory containing HTML files to use")
+                .num_args(1),
+        )
+        .get_matches();
+
+    if let Some(file) = matches.get_one::<String>("file") {
+        process_file(file);
+    } else if let Some(directory) = matches.get_one::<String>("directory") {
+        process_directory(directory);
+    } else {
+        println!("Please provide a file or directory.");
+    }
+}
+
+fn process_file(file: &str) {
+    let content = fs::read_to_string(file).expect("Could not read file");
+    let processed_content = replace_umlauts(&content);
+    fs::write(file, processed_content).expect("Could not write file");
+    println!("Processed file: {}", file);
+}
+
+fn process_directory(directory: &str) {
+    for entry in fs::read_dir(directory).expect("Could not read directory") {
+        let entry = entry.expect("Could not read entry");
+        let path = entry.path();
+        if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("html") {
+            process_file(path.to_str().expect("Could not convert path to str"));
+        }
+    }
+    println!("Processed directory: {}", directory);
+}
+
+fn replace_umlauts(content: &str) -> String {
+    content
+        .replace("ä", "&auml;")
+        .replace("ö", "&ouml;")
+        .replace("ü", "&uuml;")
+        .replace("Ä", "&Auml;")
+        .replace("Ö", "&Ouml;")
+        .replace("Ü", "&Uuml;")
+        .replace("ß", "&szlig;")
+}
